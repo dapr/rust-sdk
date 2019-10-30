def2ffa34de0f1d099d4001ee384d222538d3b10
Ryan Levick <ryan.levick@gmail.com>
2019-10-30 17:21:44 +0100

    Abstract over transport layer

--- src/client.rs
+++ src/client.rs
@@ -1,17 +1,14 @@
+use async_trait::async_trait;
 use crate::error::Error;
 use prost_types::Any;
 use std::fmt;
 
-pub struct Client(internal::client::DaprClient<tonic::transport::Channel>);
+pub struct Client<T>(T);
 
-impl Client {
+impl <T: DaprInterface> Client<T> {
     /// Connect to a Dapr enabled app.
     pub async fn connect(addr: String) -> Result<Self, Error> {
-        let (tx, rx) = tokio::sync::oneshot::channel();
-        std::thread::spawn(|| {
-            tx.send(internal::client::DaprClient::connect(addr).map(Client)).unwrap();
-        });
-        Ok(rx.await.unwrap()?)
+        T::connect(addr).await.map(Client)
     }
 
     /// Invoke a method in a Dapr enabled app.
@@ -25,23 +22,39 @@ impl Client {
         I: Into<String>,
         M: Into<String>,
     {
-        let res = self
+        self
             .0
-            .invoke_service(tonic::Request::new(internal::InvokeServiceEnvelope {
+            .invoke_service(InvokeServiceRequest {
                 id: app_id.into(),
                 method: method_name.into(),
                 data,
                 ..Default::default()
-            }))
-            .await?;
+            })
+            .await
 
-        Ok(res.into_inner())
     }
 }
 
-impl fmt::Debug for Client {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Client")
+#[async_trait]
+pub trait DaprInterface: std::marker::Sized {
+    async fn connect(addr: String) -> Result<Self, Error>;
+    async fn invoke_service(&mut self, request: InvokeServiceRequest) -> Result<InvokeServiceResponse, Error>;
+}
+
+
+#[async_trait]
+impl DaprInterface for internal::client::DaprClient<tonic::transport::Channel> {
+    async fn connect(addr: String) -> Result<Self, Error> {
+        let (tx, rx) = tokio::sync::oneshot::channel();
+        std::thread::spawn(|| {
+            tx.send(internal::client::DaprClient::connect(addr)).unwrap_or_else(|_| panic!(""));
+        });
+        Ok(rx.await.unwrap_or_else(|_| panic!(""))?)
+
+    }
+
+    async fn invoke_service(&mut self, request: InvokeServiceRequest) -> Result<InvokeServiceResponse, Error> {
+        Ok(self.invoke_service(tonic::Request::new(request)).await?.into_inner())
     }
 }
 
@@ -50,5 +63,10 @@ mod internal {
     tonic::include_proto!("dapr");
 }
 
+/// A request from invoking a service
+pub type InvokeServiceRequest = internal::InvokeServiceEnvelope;
 /// A response from invoking a service
 pub type InvokeServiceResponse = internal::InvokeServiceResponseEnvelope;
+
+/// A tonic based gRPC client
+pub type TonicClient = internal::client::DaprClient<tonic::transport::Channel>;
