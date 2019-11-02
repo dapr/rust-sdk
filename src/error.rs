9beb3804cc44cef3d2750f36f13ce0daaee4a16a
Ryan Levick <rylev@users.noreply.github.com>
2019-11-02 03:11:06 +0100

    Scaffold the Rust project (#2)
    * Beginnings of dapr client
    
    * Add very beginning of client
    
    * Small changes to public API
    
    * Make connect async
    
    * Add TBD for how to use the crate
    
    * Abstract over transport layer

---
+++ src/error.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+pub enum Error {
+    TransportError,
+    GrpcError(GrpcError),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl std::convert::From<tonic::transport::Error> for Error {
+    fn from(error: tonic::transport::Error) -> Self {
+        Error::TransportError
+    }
+}
+
+impl std::convert::From<tonic::Status> for Error {
+    fn from(error: tonic::Status) -> Self {
+        Error::GrpcError(GrpcError {})
+    }
+}
+
+#[derive(Debug)]
+pub struct GrpcError {}
+
+impl std::fmt::Display for GrpcError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod client;
+pub mod error;
+
+pub use client::Client;
