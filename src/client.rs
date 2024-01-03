2664e64e8a57fe4dbce7dbaed262c3adf0b2356c
hauju <hauke.jung@outlook.de>
2024-01-03 18:55:32 +0100

    Feature/configuration (#110)
    * Add configuration example
    
    Signed-off-by: Hauke Jung <hauke.jung@outlook.de>
    
    * Set tokio-stream as dev dependency
    
    Signed-off-by: Hauke Jung <hauke.jung@outlook.de>
    
    * cargo fmt
    
    Signed-off-by: Hauke Jung <hauke.jung@outlook.de>
    
    ---------
    
    Signed-off-by: Hauke Jung <hauke.jung@outlook.de>

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
-
 use dapr::proto::{common::v1 as common_v1, runtime::v1 as dapr_v1};
 use prost_types::Any;
+use std::collections::HashMap;
+use tonic::Streaming;
 use tonic::{async_trait, transport::Channel as TonicChannel, Request};
 
 use crate::dapr::*;
@@ -248,6 +248,62 @@ impl<T: DaprInterface> Client<T> {
     pub async fn get_metadata(&mut self) -> Result<GetMetadataResponse, Error> {
         self.0.get_metadata().await
     }
+
+    /// Get the configuration for a specific key
+    /// ///
+    /// # Arguments
+    ///
+    /// * `store_name` - The name of config store.
+    /// * `keys` - The key of the desired configuration.
+    pub async fn get_configuration<S, K>(
+        &mut self,
+        store_name: S,
+        keys: Vec<K>,
+    ) -> Result<GetConfigurationResponse, Error>
+    where
+        S: Into<String>,
+        K: Into<String>,
+    {
+        let request = GetConfigurationRequest {
+            store_name: store_name.into(),
+            keys: keys.into_iter().map(|key| key.into()).collect(),
+            metadata: Default::default(),
+        };
+        self.0.get_configuration(request).await
+    }
+
+    /// Subscribe to configuration changes
+    pub async fn subscribe_configuration<S>(
+        &mut self,
+        store_name: S,
+        keys: Vec<S>,
+    ) -> Result<Streaming<SubscribeConfigurationResponse>, Error>
+    where
+        S: Into<String>,
+    {
+        let request = SubscribeConfigurationRequest {
+            store_name: store_name.into(),
+            keys: keys.into_iter().map(|key| key.into()).collect(),
+            metadata: Default::default(),
+        };
+        self.0.subscribe_configuration(request).await
+    }
+
+    /// Unsubscribe from configuration changes
+    pub async fn unsubscribe_configuration<S>(
+        &mut self,
+        store_name: S,
+        id: S,
+    ) -> Result<UnsubscribeConfigurationResponse, Error>
+    where
+        S: Into<String>,
+    {
+        let request = UnsubscribeConfigurationRequest {
+            id: id.into(),
+            store_name: store_name.into(),
+        };
+        self.0.unsubscribe_configuration(request).await
+    }
 }
 
 #[async_trait]
@@ -269,6 +325,18 @@ pub trait DaprInterface: Sized {
     async fn delete_bulk_state(&mut self, request: DeleteBulkStateRequest) -> Result<(), Error>;
     async fn set_metadata(&mut self, request: SetMetadataRequest) -> Result<(), Error>;
     async fn get_metadata(&mut self) -> Result<GetMetadataResponse, Error>;
+    async fn get_configuration(
+        &mut self,
+        request: GetConfigurationRequest,
+    ) -> Result<GetConfigurationResponse, Error>;
+    async fn subscribe_configuration(
+        &mut self,
+        request: SubscribeConfigurationRequest,
+    ) -> Result<Streaming<SubscribeConfigurationResponse>, Error>;
+    async fn unsubscribe_configuration(
+        &mut self,
+        request: UnsubscribeConfigurationRequest,
+    ) -> Result<UnsubscribeConfigurationResponse, Error>;
 }
 
 #[async_trait]
@@ -334,6 +402,36 @@ impl DaprInterface for dapr_v1::dapr_client::DaprClient<TonicChannel> {
     async fn get_metadata(&mut self) -> Result<GetMetadataResponse, Error> {
         Ok(self.get_metadata(Request::new(())).await?.into_inner())
     }
+
+    async fn get_configuration(
+        &mut self,
+        request: GetConfigurationRequest,
+    ) -> Result<GetConfigurationResponse, Error> {
+        Ok(self
+            .get_configuration(Request::new(request))
+            .await?
+            .into_inner())
+    }
+
+    async fn subscribe_configuration(
+        &mut self,
+        request: SubscribeConfigurationRequest,
+    ) -> Result<Streaming<SubscribeConfigurationResponse>, Error> {
+        Ok(self
+            .subscribe_configuration(Request::new(request))
+            .await?
+            .into_inner())
+    }
+
+    async fn unsubscribe_configuration(
+        &mut self,
+        request: UnsubscribeConfigurationRequest,
+    ) -> Result<UnsubscribeConfigurationResponse, Error> {
+        Ok(self
+            .unsubscribe_configuration(Request::new(request))
+            .await?
+            .into_inner())
+    }
 }
 
 /// A request from invoking a service
@@ -377,6 +475,24 @@ pub type GetMetadataResponse = dapr_v1::GetMetadataResponse;
 
 /// A request for setting metadata
 pub type SetMetadataRequest = dapr_v1::SetMetadataRequest;
+
+/// A request for getting configuration
+pub type GetConfigurationRequest = dapr_v1::GetConfigurationRequest;
+
+/// A response from getting configuration
+pub type GetConfigurationResponse = dapr_v1::GetConfigurationResponse;
+
+/// A request for subscribing to configuration changes
+pub type SubscribeConfigurationRequest = dapr_v1::SubscribeConfigurationRequest;
+
+/// A response from subscribing tto configuration changes
+pub type SubscribeConfigurationResponse = dapr_v1::SubscribeConfigurationResponse;
+
+/// A request for unsubscribing from configuration changes
+pub type UnsubscribeConfigurationRequest = dapr_v1::UnsubscribeConfigurationRequest;
+
+/// A response from unsubscribing from configuration changes
+pub type UnsubscribeConfigurationResponse = dapr_v1::UnsubscribeConfigurationResponse;
 
 /// A tonic based gRPC client
 pub type TonicClient = dapr_v1::dapr_client::DaprClient<TonicChannel>;
