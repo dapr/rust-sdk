7dced71f04219613073f30e37d12733ba24edac5
Ruokun (Tommy) Niu <ruokunniu@gmail.com>
2024-04-09 14:05:15 -0700

    Feature: Enable query state API (#116)
    * init work
    
    Signed-off-by: Ruokun Niu <ruokunniu@microsoft.com>
    
    * added examples
    
    Signed-off-by: Ruokun Niu <ruokunniu@microsoft.com>
    
    * completed examples
    
    Signed-off-by: Ruokun Niu <ruokunniu@microsoft.com>
    
    * wip
    
    Signed-off-by: Ruokun Niu <ruokunniu@microsoft.com>
    
    * wip
    
    Signed-off-by: Ruokun Niu <ruokunniu@microsoft.com>
    
    * updated dataset
    
    Signed-off-by: Ruokun Niu <ruokunniu@microsoft.com>
    
    * add: validation of examples and cleanup
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    * chore: tidy
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    * chore: add query_state validation to the workflow
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    * fix: examples
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    ---------
    
    Signed-off-by: Ruokun Niu <ruokunniu@microsoft.com>
    Signed-off-by: Mike Nguyen <hey@mike.ee>
    Signed-off-by: mikeee <hey@mike.ee>
    Co-authored-by: Mike Nguyen <hey@mike.ee>

---
+++ examples/query_state/query1.rs
@@ -0,0 +1,49 @@
+use serde_json::json;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Introduce delay so that dapr grpc port is assigned before app tries to connect
+    std::thread::sleep(std::time::Duration::new(5, 0));
+
+    // Set the Dapr address and create a connection
+    let addr = "https://127.0.0.1".to_string();
+
+    // Create the client
+    let mut client = dapr::Client::<dapr::client::TonicClient>::connect(addr).await?;
+
+    let query_condition = json!({
+        "filter": {
+            "EQ": { "state": "CA" }
+        },
+        "sort": [
+            {
+                "key": "person.id",
+                "order": "DESC"
+            }
+        ]
+    });
+
+    let response = match client
+        .query_state_alpha1("statestore", query_condition, None)
+        .await
+    {
+        Ok(response) => response.results,
+        Err(e) => {
+            println!("Error: {:?}", e);
+            return Ok(());
+        }
+    };
+
+    let mut results = Vec::new();
+    for item in response {
+        let value = String::from_utf8(item.data).unwrap();
+        //push id and value as json
+        results.push(json!({
+            "id": item.key,
+            "value": value
+        }));
+    }
+    println!("Query results: {:?}", results);
+
+    Ok(())
+}
