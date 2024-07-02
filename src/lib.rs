84cd59481df0c9f3a5df859a6086f26c2b8a64cb
mikeee <hey@mike.ee>
2024-07-02 16:29:47 +0100

    docs: init
    Signed-off-by: mikeee <hey@mike.ee>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../README.md")]
+
 extern crate dapr_macros;
 
 pub use dapr_macros::actor;
@@ -6,8 +8,13 @@ pub use serde_json;
 
 pub use client::Client;
 
+/// Module containing the Dapr Callback SDK.
 pub mod appcallback;
+/// Module containing the 'Client' implementation.
 pub mod client;
+/// Module importing the Dapr runtime implementation.
 pub mod dapr;
+/// Module defining the error implementations.
 pub mod error;
+/// Module containing the 'Server' implementation.
 pub mod server;
