2ca0a901bf53f413982d21869808267f2c3f5caa
mikeee <hey@mike.ee>
2024-08-19 17:41:13 +0100

    chore: fmt
    Signed-off-by: mikeee <hey@mike.ee>

--- build.rs
+++ build.rs
@@ -6,13 +6,13 @@ fn main() -> Result<(), std::io::Error> {
         .protoc_arg("--experimental_allow_proto3_optional")
         .build_server(true)
         .compile(
-        &[
-            "dapr/proto/common/v1/common.proto",
-            "dapr/proto/runtime/v1/dapr.proto",
-            "dapr/proto/runtime/v1/appcallback.proto",
-            "examples/invoke/proto/helloworld.proto",
-        ],
-        &["."],
-    )?;
+            &[
+                "dapr/proto/common/v1/common.proto",
+                "dapr/proto/runtime/v1/dapr.proto",
+                "dapr/proto/runtime/v1/appcallback.proto",
+                "examples/invoke/proto/helloworld.proto",
+            ],
+            &["."],
+        )?;
     Ok(())
 }
