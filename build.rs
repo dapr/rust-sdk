cc2d18d33067fbb433ce5154dfdc13f56d57d0f6
Mike Nguyen <hey@mike.ee>
2024-08-19 17:54:53 +0100

    fix: add experimental proto optionals and cleanup protos for 1-rc.1 (#212)
    * fix: add experimental proto optionals and cleanup protos for 1-rc.1
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    * chore: fmt
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    ---------
    
    Signed-off-by: mikeee <hey@mike.ee>

--- build.rs
+++ build.rs
@@ -2,14 +2,17 @@
 
 fn main() -> Result<(), std::io::Error> {
     // env::set_var("OUT_DIR", "src");
-    tonic_build::configure().build_server(true).compile(
-        &[
-            "dapr/proto/common/v1/common.proto",
-            "dapr/proto/runtime/v1/dapr.proto",
-            "dapr/proto/runtime/v1/appcallback.proto",
-            "examples/invoke/proto/helloworld.proto",
-        ],
-        &["."],
-    )?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .build_server(true)
+        .compile(
+            &[
+                "dapr/proto/common/v1/common.proto",
+                "dapr/proto/runtime/v1/dapr.proto",
+                "dapr/proto/runtime/v1/appcallback.proto",
+                "examples/invoke/proto/helloworld.proto",
+            ],
+            &["."],
+        )?;
     Ok(())
 }
