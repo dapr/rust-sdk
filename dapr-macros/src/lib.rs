d8a4b97a7a0fe5d0b1cd0bfda45e0679e6f7ea16
mikeee <hey@mike.ee>
2024-08-20 16:03:08 +0100

    fix: add log dep
    Signed-off-by: mikeee <hey@mike.ee>

--- dapr-macros/src/lib.rs
+++ dapr-macros/src/lib.rs
@@ -1,10 +1,12 @@
-use proc_macro::TokenStream;
 use std::iter;
 
+use log;
 use proc_macro2::TokenTree;
 use quote::{format_ident, quote};
 use syn::parse::{Parse, ParseStream};
 use syn::{parse_macro_input, Ident, LitStr};
+
+use proc_macro::TokenStream;
 
 macro_rules! derive_parse {(
     @derive_only
