f39ff1c3b6a4662f28f6243d6c8dbc805d39b13b
Mike Nguyen <hey@mike.ee>
2026-02-10 18:06:18 +0000

    fix(sec): remove axum-test and associated indirect test vulnerabilities
    Signed-off-by: Mike Nguyen <hey@mike.ee>

--- dapr/src/server/actor/tests.rs
+++ dapr/src/server/actor/tests.rs
@@ -3,7 +3,8 @@ use std::{collections::HashMap, sync::Arc};
 
 use async_trait::async_trait;
 use axum::{Json, Router};
-use axum_test::TestServer;
+use axum::body::{Body as AxumBody, to_bytes as axum_to_bytes};
+use axum::http::Request as AxumRequest;
 use dapr::server::{
     actor::{runtime::ActorTypeRegistration, Actor, ActorError},
     DaprHttpServer,
@@ -94,19 +95,28 @@ async fn test_actor_invoke() {
     let actor_id = Uuid::new_v4().to_string();
 
     let app = dapr_server.build_test_router().await;
-    let server = TestServer::new(app.into_make_service()).unwrap();
 
-    let invoke_resp = server
-        .put(&format!("/actors/MyActor/{actor_id}/method/do_stuff"))
-        .json(&json!({ "name": "foo" }))
-        .await;
-    invoke_resp.assert_status_ok();
+    // First invoke
+    let req_body = serde_json::to_string(&json!({ "name": "foo" })).unwrap();
+    let req = AxumRequest::builder()
+        .method("PUT")
+        .uri(format!("/actors/MyActor/{actor_id}/method/do_stuff"))
+        .header("content-type", "application/json")
+        .body(AxumBody::from(req_body))
+        .unwrap();
 
-    invoke_resp.assert_json(&MyResponse {
-        actor_id: actor_id.clone(),
-        name: "foo".to_string(),
-        available: true,
-    });
+    let resp = tower::util::ServiceExt::<AxumRequest<AxumBody>>::oneshot(app.clone(), req).await.unwrap();
+    assert!(resp.status().is_success());
+    let bytes = axum_to_bytes(resp.into_body(), 64 * 1024).await.unwrap();
+    let resp_json: MyResponse = serde_json::from_slice(&bytes).unwrap();
+    assert_eq!(
+        resp_json,
+        MyResponse {
+            actor_id: actor_id.clone(),
+            name: "foo".to_string(),
+            available: true,
+        }
+    );
 
     assert_eq!(
         TEST_STATE
@@ -117,11 +127,17 @@ async fn test_actor_invoke() {
         1
     );
 
-    let invoke_resp2 = server
-        .put(&format!("/actors/MyActor/{actor_id}/method/do_stuff"))
-        .json(&json!({ "name": "foo" }))
-        .await;
-    invoke_resp2.assert_status_ok();
+    // Second invoke (should not call on_activate again)
+    let req_body2 = serde_json::to_string(&json!({ "name": "foo" })).unwrap();
+    let req2 = AxumRequest::builder()
+        .method("PUT")
+        .uri(format!("/actors/MyActor/{actor_id}/method/do_stuff"))
+        .header("content-type", "application/json")
+        .body(AxumBody::from(req_body2))
+        .unwrap();
+
+    let resp2 = tower::util::ServiceExt::<AxumRequest<AxumBody>>::oneshot(app.clone(), req2).await.unwrap();
+    assert!(resp2.status().is_success());
 
     assert_eq!(
         TEST_STATE
@@ -164,21 +180,37 @@ async fn test_actor_deactivate() {
         .await;
 
     let app = dapr_server.build_test_router().await;
-    let server = TestServer::new(app.into_make_service()).unwrap();
 
     let actor_id = Uuid::new_v4().to_string();
 
-    let invoke_resp = server
-        .put(&format!("/actors/MyActor/{actor_id}/method/do_stuff"))
-        .json(&json!({ "name": "foo" }))
-        .await;
-    invoke_resp.assert_status_ok();
+    // Invoke to activate
+    let req_body = serde_json::to_string(&json!({ "name": "foo" })).unwrap();
+    let req = AxumRequest::builder()
+        .method("PUT")
+        .uri(format!("/actors/MyActor/{actor_id}/method/do_stuff"))
+        .header("content-type", "application/json")
+        .body(AxumBody::from(req_body))
+        .unwrap();
+    let resp = tower::util::ServiceExt::<AxumRequest<AxumBody>>::oneshot(app.clone(), req).await.unwrap();
+    assert!(resp.status().is_success());
 
-    let deactivate_resp1 = server.delete(&format!("/actors/MyActor/{actor_id}")).await;
-    deactivate_resp1.assert_status_ok();
+    // Deactivate - first should succeed
+    let delete_req = AxumRequest::builder()
+        .method("DELETE")
+        .uri(format!("/actors/MyActor/{actor_id}"))
+        .body(AxumBody::empty())
+        .unwrap();
+    let delete_resp1 = tower::util::ServiceExt::<AxumRequest<AxumBody>>::oneshot(app.clone(), delete_req).await.unwrap();
+    assert!(delete_resp1.status().is_success());
 
-    let deactivate_resp2 = server.delete(&format!("/actors/MyActor/{actor_id}")).await;
-    deactivate_resp2.assert_status_not_found();
+    // Deactivate again - should be not found
+    let delete_req2 = AxumRequest::builder()
+        .method("DELETE")
+        .uri(format!("/actors/MyActor/{actor_id}"))
+        .body(AxumBody::empty())
+        .unwrap();
+    let delete_resp2 = tower::util::ServiceExt::<AxumRequest<AxumBody>>::oneshot(app.clone(), delete_req2).await.unwrap();
+    assert_eq!(delete_resp2.status().as_u16(), 404);
 
     assert_eq!(
         TEST_STATE
